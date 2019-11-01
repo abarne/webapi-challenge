a7d61327db1d1df905129bf3cf8562118187138b
abarne <celius377@gmail.com>
2019-11-01 10:31:07 -0500

    GET project by ID added

--- data/helpers/projects.js
+++ data/helpers/projects.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const projectsDb = require('./projectModel.js');
 
+// get,
+// insert,
+// update,
+// remove,
+// getProjectActions,
+
 router.get('/', (req, res) => {
 	projectsDb
 		.get()
@@ -10,6 +16,22 @@ router.get('/', (req, res) => {
 		.catch((error) => {
 			console.log(error);
 			res.status(500).json({ message: 'Error retrieving the projects.' });
+		});
+});
+
+router.get('/:id', (req, res) => {
+	const id = req.params.id;
+	projectsDb
+		.get(id)
+		.then((project) => {
+			if (!project) {
+				res.status(404).json({ message: 'No project found with specified ID' });
+			} else {
+				res.status(200).json(project);
+			}
+		})
+		.catch((error) => {
+			res.status(500).json({ message: 'Error retrieving the project' });
 		});
 });
 
